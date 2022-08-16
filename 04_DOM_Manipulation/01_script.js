9ebb73fd5c3da8c0e511d41f95c652896d818040
Rohit Kushwaha <rohitkushwahac@gmail.com>
2022-08-16 23:31:25 +0530

    guess my number project commited only files

---
+++ 04_DOM_Manipulation/01_script.js
@@ -0,0 +1,25 @@
+// DOM : Document object model
+
+// Definition : DOM is a structured representation of HTML documents which allows javascript to access HTML elements And styles to Manipulate them.
+
+// DOM is automatically created by the browers as soon as the HTML is loaded.
+
+// It is stored in tree structure format. Each HTML element in the DOM is treated as object.
+
+// Let's manipulate the DOM using Javascript
+
+// First we need to select the object(i.e HTML element)
+
+// document : The whole HTML document
+// querySelector : This selects the element given in parenthesis.
+// .textContent : gives the text of the selected Element.
+
+const score = document.querySelector('.score').textContent; // To get the text of the element we use textContent.
+console.log(score); 
+
+// changing the value of score.
+const ChangedScore = document.querySelector('.score').textContent = 4;
+
+// setting a default value into the input field
+const guessInput = document.querySelector('.guess').value = 50; // Eventhough we have defaultly added a value user can change it.
+console.log(guessInput);
