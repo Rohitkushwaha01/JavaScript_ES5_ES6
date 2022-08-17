c24aa44c02ee6a22bdf55c510fda3492be1ef337
Rohit Kushwaha <rohitkushwahac@gmail.com>
2022-08-17 23:54:29 +0530

    Guess my Number

---
+++ 04_DOM_Manipulation/2_script.js
@@ -0,0 +1,50 @@
+console.log("Guess My Number!!");
+
+// Selecting all the Elements from the DOM
+const displayMessage = document.querySelector(".message");
+const checkBtn = document.querySelector(".check");
+const scoreElement = document.querySelector(".score");
+const again = document.querySelector(".again");
+
+// Generating a random Number 
+let randomNumber = Math.trunc(Math.random()*20)+1;
+let score = 20;
+let highScore = 0;
+
+// function to display the message.
+function display(message){
+    displayMessage.textContent = message;
+}
+
+
+// handling a click event to the Check button.
+checkBtn.addEventListener('click', function(){
+    const guessInput = Number(document.querySelector(".guess").value);
+    if(!guessInput){
+        display("NO NUMBER");
+    }
+    else if (guessInput == randomNumber) {
+        display("Correct Guess!!");
+        document.querySelector('body').style.backgroundColor = "green"
+        if (score > highScore) {
+            highScore = score;
+            document.querySelector('.highscore').textContent = highScore
+        }
+    }
+    else if(guessInput < randomNumber){
+        if (score > 0) {
+            display(`Too low ${guessInput}`);
+            score--;
+            scoreElement.textContent = score;
+        }
+    }
+    else if(guessInput > randomNumber){
+        display(`Too High ${guessInput}`);
+        score--;
+        scoreElement.textContent = score;
+    }
+});
+
+again.addEventListener('click', ()=>{
+    document.location.reload();
+})
