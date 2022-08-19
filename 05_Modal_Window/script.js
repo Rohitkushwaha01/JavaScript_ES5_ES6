450c0019680372eb8cd5d5e5648d7405ff41b74e
Rohit Kushwaha <rohitkushwahac@gmail.com>
2022-08-19 20:54:57 +0530

    modal window event listener

--- 05_Modal_Window/script.js
+++ 05_Modal_Window/script.js
@@ -3,18 +3,30 @@ const modal = document.querySelectorAll(".modal");
 const closeModal = document.querySelectorAll(".close-modal");
 const addOverlay = document.querySelector(".add-overlay");
 
-
-for (let i = 0; i < showModal.length; i++) {
-
-    showModal[i].addEventListener('click', ()=>{
-        modal[i].classList.add('hidden');
-        addOverlay.classList.add('overlay');
-    });
-
-    closeModal[i].addEventListener("click", ()=>{
-        modal[i].classList.remove('hidden');
-        addOverlay.classList.remove('overlay');
-    })
+function show(i) {
+  modal[i].classList.add("hidden");
+  addOverlay.classList.add("overlay");
 }
 
+function close(i){
+    modal[i].classList.remove("hidden");
+    addOverlay.classList.remove("overlay");
+}
 
+for (let i = 0; i < showModal.length; i++) {
+  showModal[i].addEventListener("click", () => {
+    show(i);
+  });
+
+  closeModal[i].addEventListener("click", () => {
+    close(i);
+  });
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key == "Escape") {
+      if(modal[i].classList.contains('hidden')){
+        close(i);
+      }
+    }
+  });
+}
