38d0ef927494ae14294a68989284d03d96486fad
Rohit Kushwaha <rohitkushwahac@gmail.com>
2022-08-20 23:51:48 +0530

    web crawler

---
+++ 04_DOM_Manipulation/5_script.js
@@ -0,0 +1,8 @@
+const image = document.getElementById('image')
+
+// chaning the attribute
+
+image.addEventListener('click', ()=>{
+    image.src = "https://images.unsplash.com/photo-1546587348-d12660c30c50?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8bmF0dXJhbHxlbnwwfHwwfHw%3D&w=1000&q=80"
+})
+
