7961d47743e820fd8043fefe14f44fe9237ab557
Rohit Kushwaha <rohitkushwahac@gmail.com>
2022-08-27 23:26:11 +0530

    Rotate settings icon appiled if condition

--- 09_Rotate_image/script.js
+++ 09_Rotate_image/script.js
@@ -3,17 +3,28 @@ let settingIcon2 = document.getElementById('setting-2');
 let settingIcon3 = document.getElementById('setting-3');
 let angle = 1;
 
+
+function resetAngle(){
+    if(angle>360){
+        angle = 1;
+    }
+}
+
 setInterval(() => {
+    resetAngle();
     settingIcon1.style.transform = `rotate(${angle}deg)`
     angle++;
 }, 10);
 
 setInterval(() => {
+    resetAngle();
     settingIcon2.style.transform = `rotate(${angle}deg)`
     angle++;
+    console.log(angle)
 }, 10);
 
 setInterval(() => {
+    resetAngle();
     settingIcon3.style.transform = `rotate(${angle}deg)`
     angle++;
 }, 10);
