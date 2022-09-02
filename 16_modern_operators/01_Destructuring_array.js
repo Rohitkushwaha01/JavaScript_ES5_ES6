3038936fb0d337abb927448020a78b32b91f0072
Rohit Kushwaha <rohitkushwahac@gmail.com>
2022-09-02 23:17:03 +0530

    array

---
+++ 16_modern_operators/01_Destructuring_array.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const arr = [3,5,3];
+// const a = arr[0];
+// const b = arr[1];
+// const c = arr[2];
+
+// instead of declaring multiple values use the below once
+const [a1,b1,c1] = arr;
+console.log(a1,b1,c1);
+console.log(arr);
