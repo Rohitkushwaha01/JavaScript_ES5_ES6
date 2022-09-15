ec04613af9cfe6c1ed9b3c6f6e8e3cd7d546f0f2
Rohit Kushwaha <rohitkushwahac@gmail.com>
2022-09-15 20:47:07 +0530

    Maps and challenge

---
+++ 16_modern_operators/15_maps.js
@@ -0,0 +1,11 @@
+// Maps
+
+let restuarant = new Map();
+
+restuarant.set("Name", "Krishna Hotel");
+restuarant.set("1", "Mohanagar");
+restuarant.set("2", "Ramnagar");
+
+restuarant.set("Categories", ["Pizza", "Burger", "Shake"]);
+
+console.log(restuarant.get("Categories"))
