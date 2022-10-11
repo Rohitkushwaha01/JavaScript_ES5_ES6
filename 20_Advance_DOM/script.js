c941e41d3a22c849e6f6645bfb1af8efcb0839af
Rohit Kushwaha <rohitkushwahac@gmail.com>
2022-10-11 14:59:07 +0530

    fade navbar

--- 20_Advance_DOM/script.js
+++ 20_Advance_DOM/script.js
@@ -1,12 +1,20 @@
 'use strict';
 
-///////////////////////////////////////
-// Modal window
-
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1')
+
+const tabs = document.querySelectorAll(".operations__tab");
+const tabsContainer = document.querySelector(".operations__tab-container");
+const tabsContent = document.querySelectorAll(".operations__content");
+
+const nav = document.querySelector('.nav');
+///////////////////////////////////////
+// Modal Window
 
 const openModal = function () {
   modal.classList.remove('hidden');
@@ -19,7 +27,6 @@ const closeModal = function () {
 };
 
 btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
-
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 
@@ -31,9 +38,6 @@ document.addEventListener('keydown', function (e) {
 
 //////////////////////////////////////
 // Learn more button
-
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1')
 
 btnScrollTo.addEventListener('click', (e)=>{
   section1.scrollIntoView({behavior:'smooth'});
@@ -53,15 +57,6 @@ document.querySelector('.nav__links').addEventListener('click', (e)=>{
 
 //////////////////////////////////////
 // Tabbed component
-
-const tabs = document.querySelectorAll(".operations__tab");
-const tabsContainer = document.querySelector(".operations__tab-container");
-const tabsContent = document.querySelectorAll(".operations__content");
-
-console.log(tabs);
-console.log(tabsContainer);
-console.log(tabsContent)
-
 // Bad Practice: use event delegation.
 // tabs.forEach((tab)=>{
 //   tab.addEventListener('click', (e)=>{
@@ -78,7 +73,6 @@ console.log(tabsContent)
 
 tabsContainer.addEventListener('click', (e)=>{
   const clicked = e.target.closest('.operations__tab');
-  console.log(clicked);
   if(!clicked) return;
   tabs.forEach((tab)=> tab.classList.remove('operations__tab--active'));
   clicked.classList.add('operations__tab--active');
@@ -86,5 +80,45 @@ tabsContainer.addEventListener('click', (e)=>{
   // activate content area
   tabsContent.forEach((tab)=> tab.classList.remove('operations__content--active')); // removing all the class
   document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active');
-})
+});
 
+//////////////////////////////////////
+//Menu fade animation.
+
+// this function is too correct but use another one best practice
+// const handleHover = function(e, opacity){
+//   if(e.target.classList.contains('nav__link')){
+//     const link = e.target;
+//     const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+//     const logo = link.closest('.nav').querySelector('img');
+
+//     siblings.forEach(el => {
+//       if(el !== link) el.style.opacity = opacity;
+//     });
+//     logo.style.opacity = opacity;    
+//   }
+// }
+
+// nav.addEventListener('mouseover', function(e){
+//   handleHover(e, 0.5);
+// });
+
+// nav.addEventListener('mouseout', function(e){
+//   handleHover(e, 1);
+// });
+
+const handleHover = function(e){
+  if(e.target.classList.contains('nav__link')){
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if(el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;    
+  }
+}
+
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+nav.addEventListener('mouseout', handleHover.bind(1));
