37565a3611d45fe4c0dc47749fb0539acc6dfbf0
Rohit Kushwaha <rohitkushwahac@gmail.com>
2022-10-13 19:45:16 +0530

    lazing loading

--- 20_Advance_DOM/script.js
+++ 20_Advance_DOM/script.js
@@ -167,3 +167,22 @@ allSection.forEach(function(section){
   sectionObserver.observe(section);
   section.classList.add('section--hidden');
 });
+
+//////////////////////////////////////
+// lazing loading Images
+
+const imgTargets = document.querySelectorAll('img[data-src]');
+
+const loadImg = function(entries, observer){
+  const [entry] = entries;
+  if(!entry.isIntersecting) return;
+  // replacing src with data-src
+  entry.target.src = entry.target.dataset.src;
+  entry.target.addEventListener('load', function(){
+    entry.target.classList.remove('lazy-img');
+  });
+}
+
+const imgObserver = new IntersectionObserver(loadImg, {root:null, threshold:0, rootMargin:'200px'});
+
+imgTargets.forEach(img=>imgObserver.observe(img));
