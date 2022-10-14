a71d361b76c4e7c83b8cdde9d653039f24ca5bd0
Rohit Kushwaha <rohitkushwahac@gmail.com>
2022-10-14 23:02:26 +0530

    slider practice

--- 20_Advance_DOM/script.js
+++ 20_Advance_DOM/script.js
@@ -165,7 +165,7 @@ const sectionObserver = new IntersectionObserver(reavealSection, {root:null, thr
 
 allSection.forEach(function(section){
   sectionObserver.observe(section);
-  section.classList.add('section--hidden');
+  // section.classList.add('section--hidden');
 });
 
 //////////////////////////////////////
@@ -186,3 +186,53 @@ const loadImg = function(entries, observer){
 const imgObserver = new IntersectionObserver(loadImg, {root:null, threshold:0, rootMargin:'200px'});
 
 imgTargets.forEach(img=>imgObserver.observe(img));
+
+// slide
+
+const slide = document.querySelectorAll(".slide");
+const btnSliderLeft = document.querySelector('.slider__btn--left');
+const btnSliderRight= document.querySelector('.slider__btn--right');
+
+const slider = document.querySelector('.slider');
+slider.style.transform = 'scale(0.4) translateX(-800px)';
+slider.style.overflow = 'visible';
+
+let curslide = 0;
+
+// -100%, 0%, 100%, 200%
+// -200%, -100%, 0%, 100%
+// -300%, -200%, -100%, 0%
+
+btnSliderRight.addEventListener('click', ()=>{
+  if(curslide == 3){
+    curslide = 0;
+  }else{
+    curslide++;
+  }
+
+  slide.forEach((slide, i)=>{
+    slide.style.transform = `translateX(${100*(i - curslide)}%)`;
+    console.log(slide);
+  })
+});
+
+btnSliderLeft.addEventListener('click', ()=>{
+  slide.forEach((slide, i)=>{
+    slide.style.transform = `translateX(${(100*(i - curslide))}%)`;
+    console.log(slide);
+  });
+
+  // if(curslide == 3){
+  //   curslide = 0;
+  // }else{
+  //   curslide++;
+  // }
+})
+
+// 0%, 100%, 200%, 300%
+// -100%, 0%, 100%, 200%
+// -200%, -100%, 0%, 100%
+// -300%, -200%, -100%, 0%
+
+
+
