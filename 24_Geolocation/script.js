dc1e4bf428e263eff10b33d938a58116ee46d894
Rohit Kushwaha <rohitkushwahac@gmail.com>
2022-10-18 17:36:36 +0530

    Geolocation

---
+++ 24_Geolocation/script.js
@@ -0,0 +1,37 @@
+navigator.geolocation.getCurrentPosition(
+  function (position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    console.log(latitude, longitude);
+
+    const map = L.map("map").setView([latitude,longitude], 15);
+
+    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.fr/hot/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    map.on('click', function(mapEvent){
+      console.log(mapEvent);
+      const {lat, lng} = mapEvent.latlng;
+      console.log(lat, lng);
+
+      L.marker([lat, lng])
+        .addTo(map)
+        .bindPopup(
+          L.popup({
+            maxWidth: 100,
+            minWidth: 100,
+            autoClose: false,
+            closeOnClick: false,
+            className: 'running-popup',
+          })
+        )
+        .setPopupContent('Workout')
+        .openPopup();
+    })
+  },
+  function () {
+    alert("could not get you position");
+  }
+);
